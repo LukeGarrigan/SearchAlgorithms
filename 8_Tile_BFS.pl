b0005c1352db1e94c5483b67f08b06c570187da9
LukeGarrigan <lukegarrigan8@gmail.com>
2017-01-04 16:47:38 +0000

    Commented and made the program in ascending order for tiles

--- 8_Tile_BFS.pl
+++ 8_Tile_BFS.pl
@@ -1,41 +1,76 @@
-goal([2/2,1/3,2/3,3/3,3/2,3/1,2/1,1/1,1/2]).
 
+goal([3/1, 1/3, 2/3, 3/3, 1/2, 2/2, 3/2, 1/1, 2/1]).
 % Empty can move to any of its neighbours which means that 'empty' and
 % its neighbour interchange their positions
 
-move( [Empty|L], [T|L1], 1):-              % All arc costs are 1
-	swap(Empty, T, L, L1).             % Swap Empty and T in L giving L1
-
-swap(Empty, T, [T|L], [Empty|L]):-
-	d(Empty, T, 1).
-
-swap(Empty, T, [T1|L],[T1|L1]):-
-	swap(Empty, T, L, L1).
+% 0 is always at the head of the list so swaps are made there
+move( [Blank|Tiles], [Tile|New_Tiles]):-              % All arc costs are 1
+	swap(Blank, Tile, Tiles, New_Tiles).             % Swap Empty and T in L giving L1
 
 
-d(X/Y, X1/Y1, D):-
-	dif(X, X1, Dx),
-	dif(Y, Y1, Dy),
-	D is Dx+Dy.
+swap(Blank, Tile, [Tile|Tiles], [Blank|Tiles]):-          % T|L is the rest of the board
+	mandist(Blank, Tile, 1).  % Finds all the possible legal moves
+			 % The maximum difference must be one ( can't skip a tile )
 
-% Calucates the absolute difference
-dif(A, B, D):-
-	D is A-B,
-	D >= 0, !;
-	D is B-A.
+swap(Blank, Tile, [First_Tile|Tiles],[First_Tile|New_Tiles]):-
+	swap(Blank, Tile, Tiles, New_Tiles ). % takes off the head of the tail and passe
 
-
-% Figure 11.10   An implementation of breadth-first search.
+% D is the Manh. distance
+mandist( X /Y , X1 / Y1 , D ):-
+       D is abs(X - X1 )+ abs(Y - Y1 ).
 
 
-% solve( Start, Solution):
-%    Solution is a path (in reverse order) from Start to a goal
-
-
-%
 solve( Start, Solution)  :-
   breadthfirst( [ [Start] ], Sal),
   reverse(Sal, Solution).
+
+% This checks to see if the current configuration is the goal
+breadthfirst( [ [Node | Path] | _], [Node | Path])  :-
+  goal(Node).
+
+% The reason for storing all the paths is to ensure that we do not
+% choose the same path repeatedly
+
+breadthfirst( [Path | Paths], Solution)  :-
+  extend( Path, NewPaths),            % Finds all the possibilities that haven't been chosen
+  conc( Paths, NewPaths, Paths1),     % Concatenates the paths
+  breadthfirst( Paths1, Solution).    % Continues with the new paths
+
+extend( [Node | Path], NewPaths) :-
+  % Finds all the possibilities that we haven't already chosen
+  bagof( [NewNode, Node | Path],
+	  ( move( Node, NewNode), not(member( NewNode, [Node | Path]
+	  )) ),
+         NewPaths),
+  !.
+
+extend( _, [] ).              % bagof failed: Node has no successor
+
+
+conc([],L,L).
+conc([H|L1],L2,[H|L3]):-
+     conc(L1,L2,L3).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 % solve( Start, Solution)  :-
 %  breadthfirst( [ [Start] | Z] - Z, Solution).
@@ -49,33 +84,3 @@ solve( Start, Solution)  :-
 %  Paths \== Z1,                        % Set of candidates not empty
 %  breadthfirst( Paths - Z1, Solution).
 
-% This checks to see if the current configuration is the goal
-breadthfirst( [ [Node | Path] | _], [Node | Path])  :-
-  goal(Node).
-
-% Knows that it's not a goal configuration
-%
-
-
-% The reason for storing all the paths is to ensure that we do not get
-% stuck in a loop where we choose the same path repeatedly
-
-breadthfirst( [Path | Paths], Solution)  :-
-  extend( Path, NewPaths),            % Finds all the possibilities that haven't been chosen
-  conc( Paths, NewPaths, Paths1),     % Concatenates the paths
-  breadthfirst( Paths1, Solution).    % Continues with the new paths
-
-extend( [Node | Path], NewPaths) :-
-  % Finds all the possibilities that we haven't already chosen
-  bagof( [NewNode, Node | Path],
-	  ( move( Node, NewNode, _), not(member( NewNode, [Node | Path]
-	  )) ),
-         NewPaths),
-  !.
-
-extend( _, [] ).              % bagof failed: Node has no successor
-
-
-conc([],L,L).
-conc([H|L1],L2,[H|L3]):-
-conc(L1,L2,L3).
