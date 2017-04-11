be04d399127e22c0630f2630b8789906615febeb
LukeGarrigan <lukegarrigan8@gmail.com>
2017-04-11 16:24:25 +0100

    Small changes

--- SlidingTileProject/src/slidingtileproject/SlidingTileProject.java
+++ SlidingTileProject/src/slidingtileproject/SlidingTileProject.java
@@ -24,9 +24,7 @@ public class SlidingTileProject {
         int[] thirtyMoves = new int[]{5, 2, 1, 3, 7, 0, 8, 4, 9, 6, 10, 12, 13, 14, 15, 11};
         int[] thirtyFiveMoves = new int[]{5, 0, 2, 1, 7, 6, 8, 3, 9, 10, 15, 4, 13, 14, 11, 12};
         int[] fourtyMoves = new int[]{6, 7, 0, 11, 1, 5, 10, 4, 14, 13, 15, 2, 9, 8, 3, 12};
-
         int[] fiftyMoves = new int[]{1, 3, 11, 14, 6, 2, 5, 0, 15, 4, 13, 9, 12, 10, 7, 8};
-
         int[] sixtyFiveMoves = new int[]{11, 14, 9, 15, 7, 2, 8, 13, 3, 0, 5, 6, 12, 1, 10, 4};
         int[] quickTest = new int[]{2, 9, 14, 0, 4, 7, 5, 6, 15, 1, 3, 13, 10, 11, 12, 8};
 
@@ -34,9 +32,8 @@ public class SlidingTileProject {
         HeuristicFunction l = new LinearConflict();
         IterativeDeepeningAStar ida = new IterativeDeepeningAStar(l);
 
-        State initialState = new State(sixtyFiveMoves, 0, 0, null, "null");
+        State initialState = new State(twentyFiveMoves, 0, 0, null, "null");
         int intialHeuristic = l.calculateHeuristic(initialState);
-        
         initialState.setH(intialHeuristic);
 
         System.out.println("");
